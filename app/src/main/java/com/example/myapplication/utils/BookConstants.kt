801536c57e61bf3abcb1ccb08cb03c9aef7ca9a8
Oleg Suchkov <olegbeltion@gmail.com>
2022-05-27 18:19:42 +0300

    Fix base url

--- app/src/main/java/com/example/myapplication/utils/BookConstants.kt
+++ app/src/main/java/com/example/myapplication/utils/BookConstants.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.utils
 
 object BookConstants {
-//    const val BASE_URL = "https://127.0.0.1:5000/"
-    const val BASE_URL = "https://bd23-87-117-56-206.ngrok.io"
+    const val BASE_URL = "https://127.0.0.1:5000/"
 }
